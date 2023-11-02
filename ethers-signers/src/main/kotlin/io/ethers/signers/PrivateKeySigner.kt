5285d2f216778ece07f8d2958f3f9dd6fe9ef623
ArtificialPB <matej@kriptal.io>
2023-11-02 21:40:02 +0100

    feat(signers): add additional convenience constructors to PrivateKeySigner

--- ethers-signers/src/main/kotlin/io/ethers/signers/PrivateKeySigner.kt
+++ ethers-signers/src/main/kotlin/io/ethers/signers/PrivateKeySigner.kt
@@ -2,25 +2,29 @@ package io.ethers.signers
 
 import io.ethers.core.FastHex
 import io.ethers.core.types.Address
+import io.ethers.core.types.Bytes
 import io.ethers.core.types.Signature
 import io.ethers.crypto.Secp256k1
 
-class PrivateKeySigner(privateKey: String) : Signer {
+class PrivateKeySigner(privateKey: ByteArray) : Signer {
     private val signingKey: Secp256k1.SigningKey
 
     override val address: Address
 
-    init {
+    constructor(privateKey: Bytes) : this(privateKey.value)
+
+    constructor(privateKey: String) : this(FastHex.decode(privateKey)) {
         if (!FastHex.isValidHex(privateKey)) {
             throw IllegalArgumentException("Invalid private key format. Should be hex string.")
         }
+    }
 
-        val rawPrivateKey = FastHex.decode(privateKey)
-        if (rawPrivateKey.size != 32) {
-            throw IllegalArgumentException("Invalid private key length. Should be exactly 32 bytes, got ${rawPrivateKey.size}")
+    init {
+        if (privateKey.size != 32) {
+            throw IllegalArgumentException("Invalid private key length. Should be exactly 32 bytes, got ${privateKey.size}")
         }
 
-        this.signingKey = Secp256k1.SigningKey(rawPrivateKey)
+        this.signingKey = Secp256k1.SigningKey(privateKey)
         this.address = Address(Secp256k1.publicKeyToAddress(signingKey.publicKey))
     }
 
