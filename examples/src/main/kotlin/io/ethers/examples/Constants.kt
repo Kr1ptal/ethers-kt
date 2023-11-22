0adc24ef635f26df8f387bed6d6a3b755766c8e0
gaspermerela <gmerela0@gmail.com>
2023-11-22 15:41:13 +0100

    refactor(examples): abi generation and transfer ERC20

--- examples/src/main/kotlin/io/ethers/examples/Constants.kt
+++
@@ -1,9 +0,0 @@
-package io.ethers.examples
-
-import io.ethers.core.types.Address
-
-object ConstantsGoerli {
-    const val HTTP_URL = "https://ethereum-goerli.publicnode.com"
-    const val WS_URL = "wss://ethereum-goerli.publicnode.com"
-    val USDC_ADDR = Address("0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C")
-}
--- examples/src/main/kotlin/io/ethers/examples/abi/TransferERC20.kt
+++ examples/src/main/kotlin/io/ethers/examples/abi/TransferERC20.kt
@@ -1,31 +1,64 @@
 import io.ethers.core.types.Address
-import io.ethers.examples.ConstantsGoerli
 import io.ethers.providers.HttpClient
 import io.ethers.providers.Provider
 import io.ethers.signers.PrivateKeySigner
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import kotlinx.cli.required
 import java.math.BigInteger
+import java.time.Duration
 
 /**
  * This example shows how to use smart contract ABI of ERC20 token USDC (src/main/abi) to generate contract wrappers
- * automatically with abigen-plugin, and send tokens from one address to another.
+ * automatically with abigen-plugin (see build.gradle), and send tokens from one address to another.
  */
-fun main() {
-    val httpClient = HttpClient(ConstantsGoerli.HTTP_URL)
-    val provider = Provider(httpClient)
-    // Get private key and receiver wallet address from environment variables
-    val signer = PrivateKeySigner(System.getenv("W1_PRIVATE_KEY"))
-    val receiver = Address(System.getenv("W2_ADDR"))
 
-    // Initialise token contract
-    val token = ERC20(provider, ConstantsGoerli.USDC_ADDR)
-    // Submit transaction to transfer 1 USDC token
-    val pendingTransaction = token.transfer(receiver, BigInteger.valueOf(1000000))
-        .send(signer)
-        .sendAwait()
-        .resultOrThrow()
+class TransferERC20(
+    privateKey: String,
+    private val tokenAddress: String,
+    private val receiver: String,
+    private val amount: BigInteger,
+    rpcUrl: String,
+) {
+    private val httpClient = HttpClient(rpcUrl)
+    private val provider = Provider(httpClient)
 
-    // Wait for transaction to be included in a block
-    val receipt = pendingTransaction.awaitInclusion(5).resultOrThrow()
+    private val signer = PrivateKeySigner(privateKey)
+    fun run() {
+        // Initialise token contract
+        val token = ERC20(provider, Address(tokenAddress))
 
-    println("Tx ${receipt.transactionHash} was included in block ${receipt.blockNumber}")
+        println("Sending amount: $amount of token: $tokenAddress to: $receiver")
+        // Submit transaction to transfer 1 USDC token
+        val pendingTransaction = token.transfer(Address(receiver), amount)
+            .send(signer)
+            .sendAwait()
+            .resultOrThrow()
+
+        // Wait for transaction to be included in a block.
+        // Wait for tx inclusion: 2 block confirmation (default 1) for 12 retries, retry every 10 seconds (default 6 seconds)
+        val receipt = pendingTransaction.awaitInclusion(12, Duration.ofSeconds(8), 2).resultOrThrow()
+
+        println("Tx ${receipt.transactionHash} was included in block ${receipt.blockNumber}")
+    }
+}
+
+fun main(args: Array<String>) {
+    // Parse input arguments
+    val argParser = ArgParser("TransferERC20")
+
+    val privateKey by argParser.option(ArgType.String, description = "Private key").required()
+    val receiver by argParser.option(ArgType.String, description = "Address of token receiver").required()
+    val tokenAddress by argParser.option(ArgType.String, description = "Token address")
+        .default("0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C") // USDC on Goerli
+    val amount by argParser.option(ArgType.String, description = "Token amount (in lowest denomination)")
+        .default("1000000") // 1 USDC
+    val httpRpc by argParser.option(ArgType.String, description = "HTTP RPC URL")
+        .default("https://ethereum-goerli.publicnode.com") // Goerli RPC
+
+    argParser.parse(args)
+
+    val transferERC20 = TransferERC20(privateKey, tokenAddress, receiver, amount.toBigInteger(), httpRpc)
+    transferERC20.run()
 }
