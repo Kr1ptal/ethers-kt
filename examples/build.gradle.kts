eeed7109fc340459d1a736a2495e1803767a5fac
gaspermerela <gmerela0@gmail.com>
2023-11-27 13:35:56 +0100

    refactor(examples): Converting examples project to submodule

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -1,36 +1,15 @@
 plugins {
-    id("java")
-    kotlin("jvm") version "1.9.20"
+    `project-conventions`
     id("io.kriptal.ethers.abigen-plugin") version "0.1.0"
-}
-
-group = "io.ethers"
-version = "1.0-SNAPSHOT"
-
-// default values
-ethersAbigen {
-    directorySource("src/main/abi")
-    outputDir = "generated/source/ethers/main/kotlin"
-}
-
-// Define a maven repository where the library is published
-repositories {
-    mavenCentral()
-
-    // for snapshot versions, use the following repository
-    //maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 dependencies {
     // ArgParser
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
 
-    // Define a BOM and its version
-    implementation(platform("io.kriptal.ethers:ethers-bom:0.1.0"))
-
     // Define any required artifacts without version
-    implementation("io.kriptal.ethers:ethers-abi")
-    implementation("io.kriptal.ethers:ethers-core")
-    implementation("io.kriptal.ethers:ethers-providers")
-    implementation("io.kriptal.ethers:ethers-signers")
+    implementation(project(":ethers-abi"))
+    implementation(project(":ethers-core"))
+    implementation(project(":ethers-providers"))
+    implementation(project(":ethers-signers"))
 }
