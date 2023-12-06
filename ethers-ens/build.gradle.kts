74d934ca38974524bedba69879c8d9cd2125b424
gaspermerela <gmerela0@gmail.com>
2023-12-06 10:06:27 +0100

    feat(ens): Create new module ethers-ens

---
+++ ethers-ens/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    `project-conventions`
+    `maven-publish-conventions`
+    id("io.kriptal.ethers.abigen-plugin") version "0.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":ethers-core"))
+    implementation(project(":ethers-abi"))
+    implementation(project(":ethers-providers"))
+    implementation(project(":logger"))
+    implementation("io.github.adraffy:ens-normalize:0.2.0")
+
+    testImplementation(libs.bundles.junit)
+    testImplementation(libs.bundles.kotest)
+}
