29942f500dddb74104a0f42fb133a662e96c6c13
gaspermerela <gmerela0@gmail.com>
2023-12-11 16:45:42 +0100

    feat(ens): Simple ens name resolution

--- ethers-ens/src/main/kotlin/io/ethers/EnsResolver.kt
+++ ethers-ens/src/main/kotlin/io/ethers/EnsResolver.kt
@@ -1,64 +1,136 @@
 package io.ethers
 
+import io.ethers.EnsResolver.Error.FailedToResolve
+import io.ethers.EnsResolver.Error.UnknownResolver
 import io.ethers.core.ENSRegistryWithFallback
 import io.ethers.core.PublicResolver
 import io.ethers.core.types.Address
 import io.ethers.core.types.BlockId
 import io.ethers.core.types.Bytes
 import io.ethers.providers.Provider
+import io.ethers.providers.types.RpcResponse
 
 class EnsResolver(private val provider: Provider) {
 
-    fun resolveName(ensName: String): Address {
+    /**
+     * Resolve ens name to Address. On error return [RpcResponse] as error.
+     */
+    fun resolveName(ensName: String): RpcResponse<Address> {
+        // Check that ens name is valid
         if (ensName.isBlank() || (ensName.trim().length == 1 && ensName.contains("."))) {
-            return Address.ZERO
+            return RpcResponse.error(Error.EnsNameInvalid)
         }
 
-        if (isValidEnsName(ensName)) {
-            val nameHash = NameHash.nameHash(ensName)
-            val resolverAddress = getResolverAddress(nameHash)
+        val nameHashResponse = NameHash.nameHash(ensName)
+        if (nameHashResponse.isError) return nameHashResponse.propagateError()
+        val nameHash = nameHashResponse.resultOrThrow()
 
-            if (resolverAddress == Address.ZERO) {
-                throw Exception("Resolver for ens name $ensName does not exist!")
+        val resolverResponse = getResolver(nameHash)
+        if (resolverResponse.isError) return resolverResponse.propagateError()
+
+        // Unwrap resolver from RpcResponse and call its addr() function.
+        // If RpcResponse is an error, map it to error FailedToResolve.
+        val resolver = resolverResponse.resultOrThrow()
+        return resolver.addr(Bytes(nameHash))
+            .call(BlockId.LATEST)
+            .sendAwait()
+            .mapError { FailedToResolve(resolver.address, ensName, it) }
+    }
+
+    /**
+     * Get [PublicResolver] for [nameHash] using [ENSRegistryWithFallback] of current chain.
+     */
+    private fun getResolver(nameHash: ByteArray): RpcResponse<PublicResolver> {
+        return getResolverAddress(nameHash).map { PublicResolver(provider, it) }
+    }
+
+    /**
+     * Get resolver address for [nameHash] from [ENSRegistryWithFallback].
+     */
+    private fun getResolverAddress(nameHash: ByteArray): RpcResponse<Address> {
+        val registryAddr = getRegistryAddress(provider.chainId)
+            ?: return RpcResponse.error(Error.RegistryAddrNotExists(provider.chainId))
+
+        val registryContract = ENSRegistryWithFallback(provider, registryAddr)
+        return registryContract.resolver(Bytes(nameHash))
+            .call(BlockId.LATEST)
+            .sendAwait()
+            .mapError { UnknownResolver(it) }
+    }
+
+    /**
+     * Possible errors during ens name resolution
+     */
+    sealed class Error : RpcResponse.Error() {
+        /**
+         * Ens name is not valid.
+         */
+        data object EnsNameInvalid : Error()
+
+        /**
+         * Error on ens name [NameHash.normalise] attempt.
+         */
+        data class Normalisation(val cause: Exception) : Error() {
+            override fun doThrow() {
+                throw RuntimeException("Normalisation failed: $cause")
             }
-
-            val resolver = getResolver(nameHash)
-
-            try {
-                return resolver.addr(Bytes(nameHash)).call(BlockId.LATEST).sendAwait().resultOrThrow()
-            } catch (e: Exception) {
-                throw RuntimeException("Unable to execute request: ", e)
-            }
-        } else {
-            return Address(ensName)
         }
-    }
 
-    private fun isValidEnsName(ensName: String): Boolean {
-        return if (ensName.contains(".")) {
-            true
-        } else {
-            try {
-                Address(ensName)
-                false
-            } catch (e: Exception) {
-                true
+        /**
+         * Registry address does not exist for chain.
+         */
+        data class RegistryAddrNotExists(val chainId: Long) : Error() {
+            override fun doThrow() {
+                throw RuntimeException("Registry address not found for chain $chainId!")
             }
         }
-    }
 
-    private fun getResolver(nameHash: ByteArray): PublicResolver {
-        // TODO - check if node is fully synced?
-        return PublicResolver(provider, getResolverAddress(nameHash))
-    }
+        /**
+         * Resolver address not found for given nameHash on registry.
+         */
+        data class UnknownResolver(
+            val cause: RpcResponse.Error,
+        ) : Error() {
+            override fun doThrow() {
+                throw RuntimeException("Resolver for given nameHash was not found: $cause")
+            }
+        }
 
-    private fun getResolverAddress(nameHash: ByteArray): Address {
-        val registryContract = ENSRegistryWithFallback(provider, Contracts.getRegistryAddress(provider.chainId))
-        return registryContract.resolver(Bytes(nameHash)).call(BlockId.LATEST).sendAwait().resultOrThrow()
+        /**
+         * Resolver for ensName exists, but was not able to resolve it.
+         */
+        data class FailedToResolve(
+            val resolverAddr: Address,
+            val ensName: String,
+            val cause: RpcResponse.Error,
+        ) :
+            Error() {
+            override fun doThrow() {
+                throw RuntimeException("Failed to resolve ens name: $ensName with resolver $resolverAddr: $cause")
+            }
+        }
     }
 
     companion object {
-        fun Provider.resolveName(ensName: String): Address {
+        // TODO - refactor
+        private val MAINNET_REGISTRY_ADDR = Address("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e")
+        private val ROPSTEN_REGISTRY_ADDR = Address("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e")
+        private val RINKEBY_REGISTRY_ADDR = Address("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e")
+        private val GOERLI_REGISTRY_ADDR = Address("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e")
+        private val SEPOLIA_REGISTRY_ADDR = Address("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e")
+
+        fun getRegistryAddress(chainId: Long): Address? {
+            return when (chainId) {
+                1L -> MAINNET_REGISTRY_ADDR
+                3L -> ROPSTEN_REGISTRY_ADDR
+                4L -> RINKEBY_REGISTRY_ADDR
+                5L -> GOERLI_REGISTRY_ADDR
+                11155111L -> SEPOLIA_REGISTRY_ADDR
+                else -> return null
+            }
+        }
+
+        fun Provider.resolveName(ensName: String): RpcResponse<Address> {
             val ensResolver = EnsResolver(this)
             return ensResolver.resolveName(ensName)
         }
