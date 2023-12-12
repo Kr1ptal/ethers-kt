24e0012f547f76280a2b4de3952b1da681395791
gaspermerela <gmerela0@gmail.com>
2023-12-12 14:23:13 +0100

    feat(ens): New error to distinguish between registry.resolve() returning empty address and failing, improved tests

--- ethers-ens/src/test/kotlin/io/ethers/ens/EnsResolverTest.kt
+++ ethers-ens/src/test/kotlin/io/ethers/ens/EnsResolverTest.kt
@@ -1,9 +1,11 @@
 package io.ethers.ens
 
 import io.ethers.EnsResolver
+import io.ethers.EnsResolver.Companion.resolveName
 import io.ethers.core.types.Address
 import io.ethers.providers.HttpClient
 import io.ethers.providers.Provider
+import io.ethers.providers.types.RpcResponse
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
@@ -12,37 +14,84 @@ import io.kotest.matchers.types.shouldBeInstanceOf
 private const val MAINNET_HTTP_RPC = "https://ethereum.publicnode.com"
 
 class EnsResolverTest : FunSpec({
+    data class EnsNameTestData(
+        val ensName: String,
+        val nameHash: String,
+        val address: Address = Address.ZERO,
+    )
 
     context("Ens resolving with instantiated EnsResolver") {
-
+        // Todo mock provider
         val provider = Provider(HttpClient(MAINNET_HTTP_RPC))
         val ensResolver = EnsResolver(provider)
 
         context("Valid ENS names") {
             withData(
                 listOf(
-                    "resolver.eth" to Address("0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63"),
-                    "RESOLVER.eth" to Address("0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63"),
+                    EnsNameTestData(
+                        ensName = "resolver.eth",
+                        nameHash = "0x469fbad6482d86a40a35d188cb7f8256302a5d6c50e9071c4f4e9f7604b2cac8",
+                        address = Address("0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63"),
+                    ),
+                    EnsNameTestData(
+                        ensName = "rEsoLvEr.ETh",
+                        nameHash = "0x469fbad6482d86a40a35d188cb7f8256302a5d6c50e9071c4f4e9f7604b2cac8",
+                        address = Address("0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63"),
+                    ),
                 ),
             ) {
-                ensResolver.resolveName(it.first).resultOrThrow() shouldBe it.second
+                ensResolver.resolveName(it.ensName).resultOrThrow() shouldBe it.address
+
+                provider.resolveName(it.ensName).resultOrThrow() shouldBe it.address
             }
         }
 
+        // ERROR TESTING
+
+        /**
+         * Testing [EnsResolver.Error.EnsNameInvalid]
+         */
         test("Invalid ENS names") {
             ensResolver.resolveName("").error.shouldBeInstanceOf<EnsResolver.Error.EnsNameInvalid>()
             ensResolver.resolveName("\t").error.shouldBeInstanceOf<EnsResolver.Error.EnsNameInvalid>()
             ensResolver.resolveName(".").error.shouldBeInstanceOf<EnsResolver.Error.EnsNameInvalid>()
             ensResolver.resolveName("\n.").error.shouldBeInstanceOf<EnsResolver.Error.EnsNameInvalid>()
+
+            provider.resolveName("").error.shouldBeInstanceOf<EnsResolver.Error.EnsNameInvalid>()
+            provider.resolveName("\t").error.shouldBeInstanceOf<EnsResolver.Error.EnsNameInvalid>()
+            provider.resolveName(".").error.shouldBeInstanceOf<EnsResolver.Error.EnsNameInvalid>()
+            provider.resolveName("\n.").error.shouldBeInstanceOf<EnsResolver.Error.EnsNameInvalid>()
         }
 
+        /**
+         * Testing [EnsResolver.Error.Normalisation]
+         */
+        test("Failed normalisation") {
+            ensResolver.resolveName("xn--u-ccb.com").error.shouldBeInstanceOf<EnsResolver.Error.Normalisation>()
+
+            provider.resolveName("xn--u-ccb.com").error.shouldBeInstanceOf<EnsResolver.Error.Normalisation>()
+        }
+
+        /**
+         * Testing [EnsResolver.Error.UnknownResolver]
+         */
         context("Non-existent ens names") {
+            fun testError(error: RpcResponse.Error?, testData: EnsNameTestData) {
+                error.shouldBeInstanceOf<EnsResolver.Error.UnknownResolver>()
+                error.nameHash shouldBe testData.nameHash
+                error.registryAddress shouldBe EnsResolver.getRegistryAddress(provider.chainId)
+            }
+
             withData(
                 listOf(
-                    "123.kriptal.eth",
+                    EnsNameTestData(
+                        ensName = "123.kriptal.eth",
+                        nameHash = "0x469fbad6482d86a40a35d188cb7f8256302a5d6c50e9071c4f4e9f7604b2cac8",
+                    ),
                 ),
             ) {
-                ensResolver.resolveName(it).error.shouldBeInstanceOf<EnsResolver.Error.FailedToResolve>()
+                testError(ensResolver.resolveName(it.ensName).error, it)
+                testError(provider.resolveName(it.ensName).error, it)
             }
         }
     }
