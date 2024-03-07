81e9656d0ee3e79ac4d917ac164ddb5e68ea58f6
ArtificialPB <matej@kriptal.io>
2024-03-07 16:15:41 +0100

    fix(rlp): handle empty ByteArray in RlpDecodable

--- ethers-rlp/src/main/kotlin/io/ethers/rlp/RlpDecodable.kt
+++ ethers-rlp/src/main/kotlin/io/ethers/rlp/RlpDecodable.kt
@@ -1,11 +1,16 @@
 package io.ethers.rlp
 
 interface RlpDecodable<T> {
-
     /**
      * Decode [data] and return instance of [T].
      */
-    fun rlpDecode(data: ByteArray) = rlpDecode(RlpDecoder(data))
+    fun rlpDecode(data: ByteArray): T? {
+        if (data.isEmpty()) {
+            return null
+        }
+
+        return rlpDecode(RlpDecoder(data))
+    }
 
     /**
      * Decode [rlp] into an instance of [T], or null if decoding fails.
