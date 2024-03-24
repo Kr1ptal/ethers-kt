22e42d5e47c60a80f071395edaf9976fbae2e26c
ArtificialPB <matej@kriptal.io>
2024-03-24 18:36:57 +0100

    feat(core): document FeeHistory and add additional helper methods

--- ethers-core/src/main/kotlin/io/ethers/core/types/FeeHistory.kt
+++ ethers-core/src/main/kotlin/io/ethers/core/types/FeeHistory.kt
@@ -11,6 +11,19 @@ import io.ethers.core.readListOf
 import io.ethers.core.readOrNull
 import java.math.BigInteger
 
+/**
+ * This class represents a FeeHistory.
+ *
+ * @property oldestBlock The oldest block in the fee history.
+ * @property baseFeePerGas The base fee per gas for each block in the fee history. This includes the base fee for the
+ * next block as the last element. For pre-EIP-1559 blocks, the base fee is ZERO.
+ * @property gasUsedRatio The gas used ratio for each block in the fee history.
+ * @property rewards The gas tip rewards at requested percentiles for each block in the fee history. This can be null.
+ * @property baseFeePerBlobGas The base fee per blob gas for each block in the fee history. All values for a block that
+ * is empty are returned as ZERO. This can be null.
+ * @property blobGasUsedRatio The blob gas used ratio for each block in the fee history. For pre-EIP-4844 blocks, the
+ * base fee is ZERO. This can be null.
+ */
 @JsonDeserialize(using = FeeHistoryDeserializer::class)
 data class FeeHistory(
     val oldestBlock: Long,
@@ -19,7 +32,19 @@ data class FeeHistory(
     val rewards: List<List<BigInteger>>?,
     val baseFeePerBlobGas: List<BigInteger>?,
     val blobGasUsedRatio: List<Double>?,
-)
+) {
+    /**
+     * Get the next base fee per gas, or ZERO for pre-EIP-1559 blocks.
+     * */
+    val nextBaseFeePerGas: BigInteger
+        get() = baseFeePerGas.lastOrNull() ?: BigInteger.ZERO
+
+    /**
+     * Get the next base fee per blob gas, or ZERO for pre-EIP-4844 blocks.
+     * */
+    val nextBaseFeePerBlobGas: BigInteger
+        get() = baseFeePerBlobGas?.lastOrNull() ?: BigInteger.ZERO
+}
 
 class FeeHistoryDeserializer : JsonDeserializer<FeeHistory>() {
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): FeeHistory {
