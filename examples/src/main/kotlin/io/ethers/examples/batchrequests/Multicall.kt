d30168ed24bab30dd152ff4b4f12d7fb014ab741
filicode <filip@kriptal.io>
2024-04-16 11:06:17 +0200

    feat(examples): add Multicall example

---
+++ examples/src/main/kotlin/io/ethers/examples/batchrequests/Multicall.kt
@@ -0,0 +1,66 @@
+package io.ethers.examples.batchrequests
+
+import io.ethers.abi.call.Multicall3
+import io.ethers.abi.call.aggregate
+import io.ethers.core.types.Address
+import io.ethers.core.types.BlockId
+import io.ethers.examples.gen.ERC20
+import io.ethers.providers.Provider
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import java.math.BigInteger
+
+class Multicall(
+    private val rpcUrl: String,
+    private val tokens: List<Address>,
+) {
+    // Init provider
+    private val provider = Provider.fromUrl(rpcUrl).unwrap()
+
+    fun run() {
+        val aggregate = Multicall3.aggregate(
+            tokens.map { ERC20(provider, it).name() }.aggregate(),
+            tokens.map { ERC20(provider, it).symbol() }.aggregate(),
+            tokens.map { ERC20(provider, it).decimals() }.aggregate(),
+        ).call(BlockId.LATEST).sendAwait().unwrap()
+
+        val nameResults = aggregate.getAsAggregation<String>(0).unwrapOrNull()
+        val symbolResults = aggregate.getAsAggregation<String>(1).unwrapOrNull()
+        val decimalsResults = aggregate.getAsAggregation<BigInteger>(2).unwrapOrNull()
+
+        for (i in tokens.indices) {
+            println(
+                """
+            Token: ${tokens[i]}
+            Name: ${nameResults?.get(i)?.unwrap()}
+            Symbol: ${symbolResults?.get(i)?.unwrap()}
+            Decimals: ${decimalsResults?.get(i)?.unwrap()}
+            """.trimIndent() + '\n',
+            )
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    // Parse input arguments
+    val argParser = ArgParser("Multicall")
+
+    val rpcUrl by argParser.option(ArgType.String, description = "RPC URL")
+        .default("https://ethereum.publicnode.com") // Mainnet HTTP url
+
+    // Mainnet tokens
+    val tokens by argParser.option(ArgType.String, description = "List of ERC20 tokens")
+        .default(
+            "0xdAC17F958D2ee523a2206206994597C13D831ec7, " + // USDT
+                    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, " + // USDC
+                    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" // WETH
+        )
+
+    argParser.parse(args)
+
+    // Create a list of ERC20 token addresses
+    val erc20Tokens = tokens.split(", ").map { Address(it) }
+
+    Multicall(rpcUrl, erc20Tokens).run()
+}
