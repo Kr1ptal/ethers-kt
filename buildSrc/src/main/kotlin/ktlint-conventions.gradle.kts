9e0a5ca16576d68a38b5701d273f1f27f0e7e78c
ArtificialPB <matej@kriptal.io>
2024-06-23 10:55:49 +0200

    misc: move ktlint config back to gradle from editorconfig

--- buildSrc/src/main/kotlin/ktlint-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/ktlint-conventions.gradle.kts
@@ -18,4 +18,19 @@ ktlint {
     filter {
         exclude { it.file.path.contains(layout.buildDirectory.dir("generated").get().toString()) }
     }
+
+    // format of rule keys defined at: https://pinterest.github.io/ktlint/latest/rules/configuration-ktlint/#disable-rules
+    additionalEditorconfig.set(
+        mapOf(
+            "ktlint_code_style" to "intellij_idea",
+            "ktlint_standard_comment-spacing" to "disabled",
+            "ktlint_standard_discouraged-comment-location" to "disabled",
+            "ktlint_standard_property-naming" to "disabled",
+            "ktlint_standard_spacing-between-declarations-with-annotations" to "disabled",
+            "ktlint_standard_multiline-if-else" to "disabled",
+            "ktlint_standard_value-argument-comment" to "disabled",
+            "ktlint_standard_value-parameter-comment" to "disabled",
+            "ktlint_standard_backing-property-naming" to "disabled",
+        ),
+    )
 }
