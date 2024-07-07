3c592ff13230ef703ee6b1d3184060936308bef0
ArtificialPB <matej@kriptal.io>
2024-07-07 18:15:14 +0200

    misc: add depsize gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,3 +39,50 @@ allprojects {
     group = "io.kriptal.ethers"
     version = "1.0.0-SNAPSHOT"
 }
+
+subprojects {
+    /**
+     * Analyze sizes of added dependencies.
+     */
+    tasks.register("depsize", Task::class.java) {
+        doLast {
+            // skip depsize task if no runtime classpath is defined
+            val configuration = runCatching { project.configurations["runtimeClasspath"] }
+                .getOrNull() ?: return@doLast
+
+            var maxNameLength = 0
+            var totalSizeMb = 0.0
+            val formatStr = "%,10.2f"
+
+            configuration.forEach {
+                maxNameLength = maxOf(maxNameLength, it.name.trim().length)
+                totalSizeMb += it.length().toDouble() / (1024 * 1024)
+            }
+
+            val sizePadding = maxNameLength + 5
+            val newLine = System.lineSeparator()
+            val separatorLine = "".padEnd(sizePadding, '-') + newLine
+
+            val out = StringBuffer()
+            out.append(newLine)
+            out.append(separatorLine)
+            out.append("| Dependency sizes for ${project.name}")
+            out.append(newLine)
+            out.append(separatorLine)
+
+            configuration
+                .sortedBy { -it.length() }
+                .forEach {
+                    out.append("| " + it.name.padEnd(sizePadding - 2))
+                    out.append("${String.format(formatStr, (it.length() / 1024.0))} Kb$newLine")
+                }
+
+            out.append(separatorLine)
+            out.append("| Total dependencies size:".padEnd(sizePadding))
+            out.append("${String.format(formatStr, totalSizeMb)} Mb$newLine")
+            out.append(separatorLine)
+
+            println(out)
+        }
+    }
+}
